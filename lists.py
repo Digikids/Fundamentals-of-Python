da93b1ccecd0b11bd655977fbe6f81c03f3476d2
Stephen Muchendu <57831873+StephenMaina2019@users.noreply.github.com>
2021-04-11 22:02:49 +0300

    update

--- lists.py
+++ lists.py
@@ -5,8 +5,11 @@ friends = {
     "Sheddy": ["Not_Masculine", "Charming", "Creative"], 
     "George": ["Masculine", "Charming", "Not_Creative"],
 }
-
+# for key, values in friends.items():
+#     for value in friends['Max', 'Steve', 'Sheddy', 'George']:
+#         if value == "Masculine" and "Not_Charming" and "Creative":
+#             print(friends.key)
 for key, values in friends.items():
     for value in values:
-        if value == "Creative" and value == "Not_Charming":
+        if value == "Creative" and "Not_Charming" and "Masculine":
             print("Conglatulations " + key + "! you can fuck Veronica")
